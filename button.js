66e9a010e1c8fee9c8d837568569987b5f942b1e
Synchronizer <synchronizer@yandex.ru>
2024-06-14 18:17:16 +0300

    First commit

---
+++ button.js
@@ -0,0 +1,18 @@
+export class Button extends HTMLElement {
+    constructor() {
+      super();
+      this.classList.add('button');
+      if (this.getAttribute('data-icon-left')) {
+        this.innerHTML = `<b-icon class="button__icon" src='${this.getAttribute('data-icon-left')}'></b-icon> ` + this.innerHTML;
+      }
+      if (this.getAttribute('data-icon-right')) {
+        this.innerHTML += `<b-icon class="button__icon" src='${this.getAttribute('data-icon-right')}'></b-icon>`
+      }
+
+
+      if (!this.getAttribute('data-size')) {this.setAttribute('data-size', 'm')};
+  
+    }
+  }
+  
+  customElements.define("b-button", Button);
