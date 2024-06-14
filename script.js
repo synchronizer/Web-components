77f2e5064ec3b6003d5a8cd8f9fb6aaddd839143
Synchronizer <synchronizer@yandex.ru>
2024-06-14 18:36:35 +0300

    First commit

--- script.js
+++ script.js
@@ -3,26 +3,9 @@
 
 
 
-class bButton extends HTMLElement {
-    constructor() {
-      super();
-      this.classList.add('button');
-      if (this.getAttribute('data-icon-left')) {
-        this.innerHTML = `<b-icon class="button__icon" src='${this.getAttribute('data-icon-left')}'></b-icon> ` + this.innerHTML;
-      }
-      if (this.getAttribute('data-icon-right')) {
-        this.innerHTML += `<b-icon class="button__icon" src='${this.getAttribute('data-icon-right')}'></b-icon>`
-      }
-
-
-      if (!this.getAttribute('data-size')) {this.setAttribute('data-size', 'm')};
+import {Button} from './button.js'
   
-    }
-  }
-  
-  customElements.define("b-button", bButton);
-  
-  class bCheckbutton extends bButton {
+  class bCheckbutton extends Button {
     constructor() {
       super();
       this.classList.add('checkbutton')
